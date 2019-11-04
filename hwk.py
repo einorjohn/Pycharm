df03a84fa77248d5ebf15e6d209825d420529bea
einorjohn <54127861+einorjohn@users.noreply.github.com>
2019-11-04 22:27:24 +0800

    Delete hwk.py

--- hwk.py
+++
@@ -1,9 +0,0 @@
-string = input("Input Word: ")
-char = input("Input Character: ")
-
-count=0
-for i in range(len(string)):
-    if(string[i] == char):
-        count = count + 1
-
-print("No. of times",char, "was present in", string, ":", count )
