ff9b5b6bb0db3f27931e418fe02381a729e381b9
einorjohn <54127861+einorjohn@users.noreply.github.com>
2019-11-08 21:13:36 +0800

    Update Lec 8 Ex. 4.py

--- Lec 8 Ex. 4.py
+++ Lec 8 Ex. 4.py
@@ -1,11 +1,18 @@
 collect = []
 while True:
-    inp = input('Enter a number: ')
-    if inp =='done':
+    inp= input('Enter a number: ')
+    if inp == 'done':
         break
-    value = float(inp)
-    collect.append(value)
-print('Maximum is: ', max(collect))
-print('Minimum is: ', min(collect))
+    try:
+        inp = float(inp)
+    except:
+        print('Please input a numerical value')
+        continue
+    collect.append(inp)
+if len(collect) == 0:
+    print('Please enter a numerical value first!')
+else:
+    print('Maximum is: ', max(collect))
+    print('Minimum is: ', min(collect))
 
 
